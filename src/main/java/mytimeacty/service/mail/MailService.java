c3f9731b217887d2b78be9e4df91888c94f68022
Stefan <stefan.mircovici@student.vinci.be>
2024-08-22 15:54:48 +0200

    add - profiles dev and prod

--- src/main/java/mytimeacty/service/mail/MailService.java
+++ src/main/java/mytimeacty/service/mail/MailService.java
@@ -3,6 +3,8 @@ package mytimeacty.service.mail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -14,21 +16,28 @@ public class MailService {
     @Autowired
     private JavaMailSender mailSender;
     
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+    
     private static final Logger logger = LoggerFactory.getLogger(MailService.class);
     
     public void sendVerificationEmail(String toEmail, String token) {
-        try {
-            String verificationLink = "https://www.mytimeacty.com/verify?token=" + token;
-            
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(toEmail);
-            message.setSubject("Please Verify Your Email");
-            message.setText("Thank you for registering. Please click the link below to verify your account:\n" + verificationLink);
-            
-            mailSender.send(message);
-            logger.info("Method sendVerificationEmail: Verification email sent to {}", toEmail);
-        } catch (Exception e) {
-            logger.error("Method sendVerificationEmail: Failed to send verification email to {}", toEmail, e);
+    	if ("prod".equals(activeProfile)) {
+	        try {
+	            String verificationLink = "https://www.mytimeacty.com/verify?token=" + token;
+	            
+	            SimpleMailMessage message = new SimpleMailMessage();
+	            message.setTo(toEmail);
+	            message.setSubject("Please Verify Your Email");
+	            message.setText("Thank you for registering. Please click the link below to verify your account:\n" + verificationLink);
+	            
+	            mailSender.send(message);
+	            logger.info("Method sendVerificationEmail: Verification email sent to {}", toEmail);
+	        } catch (Exception e) {
+	            logger.error("Method sendVerificationEmail: Failed to send verification email to {}", toEmail, e);
+	        }
+    	} else {
+            System.out.println("Mode non-production : email not sent");
         }
     }
 }
